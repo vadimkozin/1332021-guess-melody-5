6c2b42cf9a9c36c09ea3ca7ddecb87b2f536feb6
Vadim Kozin <ulin.salma@yandex.ru>
2020-10-02 19:56:08 +0300

    добавил маршрутизацию

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {Route, BrowserRouter} from "react-router-dom";
+import ArtistQuestionScreen from '../artist-question-screen/artist-question-screen';
+import AuthScreen from '../auth-screen/auth-screen';
+import GameOverScreen from '../game-over-screen/game-over-screen';
+import GenreQuestionScreen from '../genre-question-screen/genre-question-screen';
+import WinScreen from '../win-screen/win-screen';
 import WelcomeScreen from '../welcome-screen/welcome-screen';
+import NotFound from '../not-found/not-found';
 
-const App = ({errorCount}) => (
-  <WelcomeScreen errorCount={errorCount}></WelcomeScreen>
-);
+const App = ({errorCount}) => {
+  return (
+    <BrowserRouter>
+      <Route exact path="/" render={(props) => <WelcomeScreen errorCount={errorCount} {...props} />}></Route>
+      <Route exact path='/dev-artist' component={ArtistQuestionScreen}></Route>
+      <Route exact path='/dev-genre' component={GenreQuestionScreen}></Route>
+      <Route exact path='/login' component={AuthScreen}></Route>
+      <Route exact path='/result' component={WinScreen}></Route>
+      <Route exact path='/lose' component={GameOverScreen}></Route>
+      <Route component={NotFound}></Route>
+    </BrowserRouter>
+  );
+};
 
 App.propTypes = {
   errorCount: PropTypes.number.isRequired
