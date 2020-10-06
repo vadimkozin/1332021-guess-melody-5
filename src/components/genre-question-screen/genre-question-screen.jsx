4307e9aeebda734351d85e7a34f2c302596165b4
Vadim Kozin <ulin.salma@yandex.ru>
2020-10-06 16:10:02 +0300

    добавил формирование формы для игры по жанру

--- src/components/genre-question-screen/genre-question-screen.jsx
+++ src/components/genre-question-screen/genre-question-screen.jsx
@@ -16,6 +16,30 @@ class GenreQuestionScreen extends React.PureComponent {
     const {answers, genre} = question;
     const {answers: userAnswers} = this.state;
 
+    const trackList = answers.map((answer, i) => (
+      <div key={`${i}-${answer.src}`} className="track">
+        <button className="track__button track__button--play" type="button"></button>
+        <div className="track__status">
+          <audio src={answer.src}></audio>
+        </div>
+        <div className="game__answer">
+          <input className="game__input visually-hidden" type="checkbox" name="answer"
+            value={`answer-${i}`}
+            id={`answer-${i}`}
+            checked={userAnswers[i]}
+            onChange={(evt) => {
+              const value = evt.target.checked;
+
+              this.setState({
+                answers: [...userAnswers.slice(0, i), value, ...userAnswers.slice(i + 1)]
+              });
+            }}
+          />
+          <label className="game__check" htmlFor={`answer-${i}`}>Отметить</label>
+        </div>
+      </div>
+    ));
+
     return (
       <section className="game game--genre">
         <header className="game__header">
@@ -35,52 +59,9 @@ class GenreQuestionScreen extends React.PureComponent {
         </header>
 
         <section className="game__screen">
-          <h2 className="game__title">Выберите инди-рок треки</h2>
+          <h2 className="game__title">Выберите {genre} треки</h2>
           <form className="game__tracks">
-            <div className="track">
-              <button className="track__button track__button--play" type="button"></button>
-              <div className="track__status">
-                <audio></audio>
-              </div>
-              <div className="game__answer">
-                <input className="game__input visually-hidden" type="checkbox" name="answer" value="answer-1" id="answer-1" />
-                <label className="game__check" htmlFor="answer-1">Отметить</label>
-              </div>
-            </div>
-
-            <div className="track">
-              <button className="track__button track__button--play" type="button"></button>
-              <div className="track__status">
-                <audio></audio>
-              </div>
-              <div className="game__answer">
-                <input className="game__input visually-hidden" type="checkbox" name="answer" value="answer-1" id="answer-2" />
-                <label className="game__check" htmlFor="answer-2">Отметить</label>
-              </div>
-            </div>
-
-            <div className="track">
-              <button className="track__button track__button--pause" type="button"></button>
-              <div className="track__status">
-                <audio></audio>
-              </div>
-              <div className="game__answer">
-                <input className="game__input visually-hidden" type="checkbox" name="answer" value="answer-1" id="answer-3" />
-                <label className="game__check" htmlFor="answer-3">Отметить</label>
-              </div>
-            </div>
-
-            <div className="track">
-              <button className="track__button track__button--play" type="button"></button>
-              <div className="track__status">
-                <audio></audio>
-              </div>
-              <div className="game__answer">
-                <input className="game__input visually-hidden" type="checkbox" name="answer" value="answer-1" id="answer-4" />
-                <label className="game__check" htmlFor="answer-4">Отметить</label>
-              </div>
-            </div>
-
+            {trackList}
             <button className="game__submit button" type="submit">Ответить</button>
           </form>
         </section>
