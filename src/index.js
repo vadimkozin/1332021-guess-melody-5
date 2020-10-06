3ec6bee38f6ce6b0bacb16099e9736a8a1a31ffa
Vadim Kozin <ulin.salma@yandex.ru>
2020-10-06 14:27:02 +0300

    подключил моки, передал их в App

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
+import {questions} from './mocks/questions';
 
 const Settings = {
   ERROR_COUNT: 3,
 };
 
 ReactDOM.render(
-    <App errorCount={Settings.ERROR_COUNT}></App>,
+    <App
+      errorCount={Settings.ERROR_COUNT}
+      questions={questions}
+    />,
     document.querySelector(`#root`)
 );
