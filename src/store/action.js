8e989fd0585b2378d7c2bd8266d4bb27860c4aa0
Vadim Kozin <ulin.salma@yandex.ru>
2020-10-20 10:35:42 +0300

    Добавит новое действие incrementMistake

--- src/store/action.js
+++ src/store/action.js
@@ -1,4 +1,5 @@
-import {STEP_DEFAULT} from '../const';
+import {isArtistAnswerCorrect, isGenreAnswerCorrect} from "../game";
+import {GameType} from "../const";
 
 export const ActionType = {
   INCREMENT_MISTAKES: `INCREMENT_MISTAKES`,
@@ -9,9 +10,26 @@ export const ActionType = {
 export const ActionCreator = {
   incrementStep: () => ({
     type: ActionType.INCREMENT_STEP,
-    payload: STEP_DEFAULT,
+    payload: 1,
   }),
   resetGame: () => ({
     type: ActionType.RESET_GAME,
-  })
+  }),
+  incrementMistake: (question, userAnswer) => {
+    let answerIsCorrect = false;
+
+    switch (question.type) {
+      case GameType.ARTIST:
+        answerIsCorrect = isArtistAnswerCorrect(question, userAnswer);
+        break;
+      case GameType.GENRE:
+        answerIsCorrect = isGenreAnswerCorrect(question, userAnswer);
+        break;
+    }
+
+    return {
+      type: ActionType.INCREMENT_MISTAKES,
+      payload: answerIsCorrect ? 0 : 1,
+    };
+  },
 };
